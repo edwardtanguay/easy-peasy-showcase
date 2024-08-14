14b9d75ce6fe36567754da5243dc3d386499b6c3
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-15 00:22:24 +0200

    shadow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header";
 
 function App() {
 	return (
-		<main className="bg-gray-400 rounded-lg p-4 w-full md:w-[60rem] mt-0 md:mt-6">
+		<main className="bg-gray-400 rounded-lg p-4 w-full md:w-[60rem] mt-0 md:mt-6 shadow-lg">
 			<Header />
 			<main className="py-4">
 				<Outlet />
