7fed52314a8c0ba355581727b8abaa1c8812b93e
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-24 19:28:46 +0200

    third

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,19 @@
-import { createStore } from 'easy-peasy';
-import { StoreModel } from './types';
+import { createStore } from "easy-peasy";
+import { StoreModel } from "./types";
 
 export const store = createStore<StoreModel>({
-  flashcards: [
-    {
-      front: "the paper",
-      back: "das Papier"
-    },
-    {
-      front: "the fork",
-      back: "die Gabel"
-    }
-  ],
+	flashcards: [
+		{
+			front: "the paper",
+			back: "das Papier",
+		},
+		{
+			front: "the fork",
+			back: "die Gabel",
+		},
+		{
+			front: "the knife",
+			back: "das Messer",
+		},
+	],
 });
