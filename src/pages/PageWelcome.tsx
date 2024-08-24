31e667e3bee86e21aaf55e988d5925fa12d086c2
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-24 21:39:56 +0200

    button is quasi working

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,4 +1,4 @@
-import { useStoreState } from "../hooks";
+import { useStoreState, useStoreActions } from "../hooks";
 // import { Flashcard } from "../types";
 // import { StoreModel } from "../store";
 
@@ -6,7 +6,7 @@ export const PageWelcome = () => {
 
 	const flashcards = useStoreState((state) => state.flashcards);
 	// const flashcards = useStoreState<StoreModel, Flashcard[]>((state) => state.flashcards);
-	// const deleteFlashcard = useStoreActions((actions) => actions.deleteFlashcard);
+	const deleteFlashcard = useStoreActions((actions) => actions.deleteFlashcard);
 	// const deleteFlashcard = useStoreActions<StoreModel, (id: number) => void>;
 
 	return (
@@ -15,8 +15,7 @@ export const PageWelcome = () => {
 			<ul className="list-disc ml-6">
 				{flashcards.map((flashcard, index) => {
 					return (
-						<li key={index}>{flashcard.front} - {flashcard.back}</li>
-						//  <button onClick={() => deleteFlashcard(state, flashcard.id)}>delete</button></li>
+						<li key={index}>{flashcard.front} - {flashcard.back} <button onClick={() => deleteFlashcard(flashcard.id)}>delete</button></li>
 					)
 				})}
 			</ul>
