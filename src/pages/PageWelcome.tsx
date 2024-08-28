27daffcf413993244dbf871c7a1797ae6a7da89b
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-28 10:58:56 +0200

    new data

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -4,7 +4,7 @@ import { FaRegTrashCan } from "react-icons/fa6";
 export const PageWelcome = () => {
 
 	const { title, flashcards } = useStoreState((state) => state.flashcardModel);
-	const { setTitle, deleteFlashcard,deleteFlashcardThunk } = useStoreActions((actions) => actions.flashcardModel);
+	const { setTitle, deleteFlashcardThunk } = useStoreActions((actions) => actions.flashcardModel);
 
 	const handleChangeTitle = () => {
 		setTitle("new title");
@@ -18,10 +18,7 @@ export const PageWelcome = () => {
 				{flashcards.map((flashcard, index) => {
 					return (
 						<li key={index}>
-							<div className="mt-2 flex gap-2">{flashcard.front} - {flashcard.back} <FaRegTrashCan className="mt-1 hover:text-red-800 cursor-pointer" onClick={() => deleteFlashcard(flashcard.id)} />
-
-- <span className="cursor-pointer" onClick={() => deleteFlashcardThunk(flashcard.id)}>delete in JSON file</span>
-
+							<div className="mt-2 flex gap-2">{flashcard.front} - {flashcard.back} <FaRegTrashCan className="mt-1 hover:text-red-800 cursor-pointer" onClick={() => deleteFlashcardThunk(flashcard.id)} />
 							</div>
 						</li>
 					)
