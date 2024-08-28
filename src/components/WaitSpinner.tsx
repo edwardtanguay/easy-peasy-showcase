a94e25b90f98f35cff7c6eff5151c4cc05964455
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-28 16:09:13 +0200

    word waiting

---
+++ src/components/WaitSpinner.tsx
@@ -0,0 +1,3 @@
+export const WaitSpinner = () => {
+	return 'loading...'
+}
--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -1,3 +1,4 @@
+import { WaitSpinner } from "../components/WaitSpinner";
 import { useStoreState, useStoreActions } from "../store/hooks"
 import { FaRegTrashCan } from "react-icons/fa6";
 
@@ -8,18 +9,24 @@ export const PageFlashcards = () => {
 
 	return (
 		<>
-			<p>There are {flashcards.length} flashcards.</p>
-			<p>Loading status: {flashcardLoadingStatus}</p>
-			<ul className="list-disc ml-6">
-				{flashcards.map((flashcard, index) => {
-					return (
-						<li key={index}>
-							<div className="mt-2 flex gap-2">{flashcard.front} - {flashcard.back} <FaRegTrashCan className="mt-1 hover:text-red-800 cursor-pointer" onClick={() => deleteFlashcardThunk(flashcard.id)} />
-							</div>
-						</li>
-					)
-				})}
-			</ul>
+			{flashcardLoadingStatus === "loading" ? (
+				<WaitSpinner />
+			) : (
+				<>
+					<p>There are {flashcards.length} flashcards.</p>
+					<p>Loading status: {flashcardLoadingStatus}</p>
+					<ul className="list-disc ml-6">
+						{flashcards.map((flashcard, index) => {
+							return (
+								<li key={index}>
+									<div className="mt-2 flex gap-2">{flashcard.front} - {flashcard.back} <FaRegTrashCan className="mt-1 hover:text-red-800 cursor-pointer" onClick={() => deleteFlashcardThunk(flashcard.id)} />
+									</div>
+								</li>
+							)
+						})}
+					</ul>
+				</>
+			)}
 		</>
 	)
 }
