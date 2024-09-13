669163a5cf3ec6babb106be55e436660fb09c72a
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-13 15:48:20 +0200

    basic two mode

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,15 +5,20 @@ export const Nav = () => {
 	const { showInfoPage } = useStoreState((state) => state.settingsModel);
 
 	return <nav>
-		<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
-			<li><NavLink to="/welcome">Welcome</NavLink></li>
-			<li><NavLink to="/settings">Settings</NavLink></li>
-			<li><NavLink to="/flashcards">Flashcards</NavLink></li>
-			<li><NavLink to="/mui">MUI</NavLink></li>
-			<li><NavLink to="/learniverse">Learniverse</NavLink></li>
-			{showInfoPage && (
-				<li><NavLink to="/info">Info</NavLink></li>
-			)}
-		</ul>
+		<div className="md:hidden">
+			<p>SMARTPHONE VIEW</p>
+		</div>
+		<div className="hidden md:block">
+			<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
+				<li><NavLink to="/welcome">Welcome</NavLink></li>
+				<li><NavLink to="/settings">Settings</NavLink></li>
+				<li><NavLink to="/flashcards">Flashcards</NavLink></li>
+				<li><NavLink to="/mui">MUI</NavLink></li>
+				<li><NavLink to="/learniverse">Learniverse</NavLink></li>
+				{showInfoPage && (
+					<li><NavLink to="/info">Info</NavLink></li>
+				)}
+			</ul>
+		</div>
 	</nav>;
 };
