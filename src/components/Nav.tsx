6de4ff0c7ad88c20046fa39ec252c14be77990ab
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-13 16:04:44 +0200

    menu generated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,51 @@
 import { useStoreState } from "../store/hooks"
 import { NavLink } from "react-router-dom";
 
+const menuItems = [
+	{
+		idCode: 'welcome',
+		title: 'Welcome'
+	},
+	{
+		idCode: 'settings',
+		title: 'Settings'
+	},
+	{
+		idCode: 'flashcards',
+		title: 'Flashcards'
+	},
+	{
+		idCode: 'mui',
+		title: 'MUI'
+	},
+	{
+		idCode: 'learniverse',
+		title: 'Learniverse'
+	},
+	{
+		idCode: 'info',
+		title: 'Info'
+	}
+]
+
 export const Nav = () => {
 	const { showInfoPage } = useStoreState((state) => state.settingsModel);
 
 	return <nav>
-		<div className="md:hidden md:block bg-slate-500 px-4 py-2 content">
+		<div className="md:hidden bg-slate-500 px-4 py-2 content">
 			<p>SMARTPHONE VIEW</p>
 		</div>
 		<div className="hidden md:block bg-slate-500 px-4 py-2 content">
 			<ul className="flex gap-4">
-				<li><NavLink to="/welcome">Welcome</NavLink></li>
-				<li><NavLink to="/settings">Settings</NavLink></li>
-				<li><NavLink to="/flashcards">Flashcards</NavLink></li>
-				<li><NavLink to="/mui">MUI</NavLink></li>
-				<li><NavLink to="/learniverse">Learniverse</NavLink></li>
-				{showInfoPage && (
-					<li><NavLink to="/info">Info</NavLink></li>
-				)}
+				{menuItems.map((menuItem, index) => {
+					return (
+						<>
+							{((menuItem.idCode !== 'info' || (menuItem.idCode === 'info' && showInfoPage)) && (
+								<li key={index}><NavLink to={menuItem.idCode}>{menuItem.title}</NavLink></li>
+							))}
+						</>
+					)
+				})}
 			</ul>
 		</div>
 	</nav>;
