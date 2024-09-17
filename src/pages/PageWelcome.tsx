40dfe34ff30ea85460f8dcdcad524aeedd735275
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-17 14:46:43 +0200

    online site

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -5,15 +5,19 @@ export const PageWelcome = () => {
 		<section className="page pageWelcome">
 			<p className="mb-1">This site is a showcase for new tech I am learning:</p>
 			<ul className="list-disc ml-6">
-				<li>notes are here: <a href="https://tanguay-eu.vercel.app/forays/321" target="_blank" className="underline">https://tanguay-eu.vercel.app/forays/321</a></li>
-				<li>repo is here: <a href="https://github.com/edwardtanguay/newtech2024" target="_blank" className="underline">https://github.com/edwardtanguay/newtech2024</a></li>
+				<li><span className="font-semibold">notes</span> are here: <a href="https://tanguay-eu.vercel.app/forays/321" target="_blank" className="underline">https://tanguay-eu.vercel.app/forays/321</a></li>
+				<li><span className="font-semibold">repo</span> is here: <a href="https://github.com/edwardtanguay/newtech2024" target="_blank" className="underline">https://github.com/edwardtanguay/newtech2024</a></li>
+				<li><span className="font-semibold">online site</span> is here: <a href="https://newtech2024.vercel.app" target="_blank" className="underline">https://newtech2024.vercel.app</a></li>
+
+
+
 			</ul>
 
 			<p className="mb-1 mt-6">New tech:</p>
 			<ul className="list-disc ml-6">
 				<li><b>easy-peasy</b> - <i>Redux-like state management without the boilerplate, created 2019</i>
 					<ul className="list-disc ml-6">
-						<li>see pages <NavLink className="underline" to='/settings'>Settings</NavLink> and <NavLink className="underline" to='/flashcards'>Flashcards</NavLink> (flashcard site only works offline since it runs on json-server)</li>
+						<li>see pages <NavLink className="underline" to='/settings'>Settings</NavLink> and <NavLink className="underline" to='/flashcards'>Flashcards</NavLink> (flashcard site only works offline since it runs on a json-server backend API)</li>
 						<li>see examples of thunks</li>
 						<li>also: store is split up into two models</li>
 						<li><a target="_blank" href="https://easy-peasy.vercel.app/" className="underline">easy-peasy documentation</a></li>
