3ebc405f2f256bc304626e5dc4c13ad1e9104175
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-17 08:07:15 +0200

    container

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { useStoreActions } from "./store/hooks"
 import { useEffect } from "react";
+import { Container } from "@mui/material";
 
 
 function App() {
@@ -13,12 +14,12 @@ function App() {
 		loadAllFlashcardsThunk()
 	}, [])
 	return (
-		<main className="bg-gray-400 rounded-lg p-4 w-full md:w-[60rem] mt-0 md:mt-6">
-			<Header />
-			<main className="py-4">
-				<Outlet />
-			</main>
-		</main>
+		<Container className="bg-gray-400 rounded-lg w-full py-4 md:w-[60rem] mt-0 md:mt-6">
+				<Header />
+				<main className="py-4">
+					<Outlet />
+				</main>
+		</Container>
 	);
 }
 
--- src/pages/PageMui.tsx
+++ src/pages/PageMui.tsx
@@ -1,21 +1,36 @@
-import { Box, Button, ButtonGroup } from '@mui/material';
+import { Box, Button, ButtonGroup, Container } from '@mui/material';
 import { Typography } from "@mui/material";
 import { SectionHeader } from '../components/SectionHeader';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import ConstructionOutlinedIcon from '@mui/icons-material/ConstructionOutlined';
 
 export const PageMui = () => {
 	return (
 		<section className='page pageMui'>
 			<SectionHeader title="Typography" marginTop='0'></SectionHeader>
-			<div>
+			<Container>
 				{/* Heading Variants */}
 				<Typography
 					variant="h1"
 					color="primary"
-					align="center"
-				>H1 Primary </Typography>
+					className='hidden md:block'>H1 Primary</Typography>
+				<Typography
+					variant="h1"
+					color="primary"
+					className='md:hidden p-0 m-0'
+					sx={{ lineHeight: "3.5rem" }}
+				><span className='text-[3.5rem]'>H1 Primary</span></Typography>
+
 				<Typography variant="h2"
 					color="secondary"
-				>H2 Secondary </Typography>
+					className='hidden md:block'>H2 Primary</Typography>
+				<Typography
+					variant="h2"
+					color="secondary"
+					className='md:hidden p-0 m-0'
+					sx={{ lineHeight: "3rem" }}
+				><span className='text-[3rem]'>H2 Primary</span></Typography>
+
 				<Typography variant="h3" color="error">H3 Error </Typography>
 				<Typography variant="h4" color="warning">H4 Warning </Typography>
 				<Typography variant="h5" color="info">H5 Info </Typography>
@@ -39,10 +54,10 @@ export const PageMui = () => {
 
 				{/* Other Variants */}
 				This is <Typography variant="caption">Caption Text</Typography> and this is <Typography variant="overline">Overline Text</Typography> and this is <Typography variant="button">Button Text</Typography>.
-			</div>
+			</Container>
 
 			<SectionHeader title="Buttons"></SectionHeader>
-			<section className='flex gap-2 flex-wrap'>
+			<Container className='flex gap-2 flex-wrap'>
 				<Button variant="contained">Default (no color)</Button>
 				<Button variant="contained" color="primary">Primary</Button>
 				<Button variant="contained" color="secondary">Secondary</Button>
@@ -55,7 +70,7 @@ export const PageMui = () => {
 				<Button variant="contained" color="success" disabled>Success Disabled</Button>
 				<Button variant="outlined" color="success">Success</Button>
 				<Button variant="outlined" color="error">Error</Button>
-			</section>
+			</Container>
 
 			<SectionHeader title="Button Groups"></SectionHeader>
 			<section className='flex gap-2 flex-wrap'>
@@ -65,11 +80,40 @@ export const PageMui = () => {
 					<Button>Copy</Button>
 				</ButtonGroup>
 				<ButtonGroup variant='contained' disableRipple>
-					<Button color="success">Save</Button>
-					<Button color="error">Delete</Button>
-					<Button color="warning">Copy</Button>
+					<Button
+						sx={{
+							backgroundColor: 'success.main',
+							'&:hover': {
+								backgroundColor: 'success.dark',
+							},
+						}}
+					>Save</Button>
+					<Button
+						sx={{
+							backgroundColor: 'error.main',
+							'&:hover': {
+								backgroundColor: 'error.dark',
+							},
+						}}
+					>Delete</Button>
+					<Button
+						sx={{
+							backgroundColor: 'warning.main',
+							'&:hover': {
+								backgroundColor: 'warning.dark',
+							},
+						}}
+					>Copy</Button>
 				</ButtonGroup>
 			</section>
+
+			<SectionHeader title="Icons"></SectionHeader>
+			<Container>
+				<ConstructionIcon />
+				<ConstructionOutlinedIcon color="secondary"/>
+				<ConstructionOutlinedIcon color="primary" fontSize="large"/>
+				<ConstructionOutlinedIcon color="primary" fontSize="small"/>
+			</Container>
 
 			<SectionHeader title="Boxes"></SectionHeader>
 			<Box sx={{
