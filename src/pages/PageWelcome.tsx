c040638342202a7fa8e383ae30a2273eb6bf0ac0
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-17 15:17:07 +0200

    all text in

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,19 +1,19 @@
 import { NavLink } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export const PageWelcome = () => {
 	return (
 		<section className="page pageWelcome">
 			<p className="mb-1">This site is a showcase for new tech I am learning:</p>
 			<ul className="list-disc ml-6">
-				<li><span className="font-semibold">notes</span> are here: <a href="https://tanguay-eu.vercel.app/forays/321" target="_blank" className="underline">https://tanguay-eu.vercel.app/forays/321</a></li>
-				<li><span className="font-semibold">repo</span> is here: <a href="https://github.com/edwardtanguay/newtech2024" target="_blank" className="underline">https://github.com/edwardtanguay/newtech2024</a></li>
-				<li><span className="font-semibold">online site</span> is here: <a href="https://newtech2024.vercel.app" target="_blank" className="underline">https://newtech2024.vercel.app</a></li>
-
-
-
+				<li><span className="font-semibold">notes</span> are here <FaArrowRightLong className="inline-block" /> <a href="https://tanguay-eu.vercel.app/forays/321" target="_blank" className="underline">https://tanguay-eu.vercel.app/forays/321</a></li>
+				<li><span className="font-semibold">repo</span> is here <FaArrowRightLong className="inline-block" />  <a href="https://github.com/edwardtanguay/newtech2024" target="_blank" className="underline">https://github.com/edwardtanguay/newtech2024</a></li>
+				<li><span className="font-semibold">online site</span> is here <FaArrowRightLong className="inline-block" /> <a href="https://newtech2024.vercel.app" target="_blank" className="underline">https://newtech2024.vercel.app</a></li>
 			</ul>
 
-			<p className="mb-1 mt-6">New tech:</p>
+			<p className="mt-4 text-orange-900">The goal of this project is to learn these technologies in a real-world way by getting them all to work together on this site, with responsive UX, etc.</p>
+
+			<p className="mb-1 mt-4">The technological line-up:</p>
 			<ul className="list-disc ml-6">
 				<li><b>easy-peasy</b> - <i>Redux-like state management without the boilerplate, created 2019</i>
 					<ul className="list-disc ml-6">
@@ -43,6 +43,32 @@ export const PageWelcome = () => {
 					</ul>
 				</li>
 			</ul>
+
+			<ul className="list-disc ml-6 mt-3">
+				<li><b>ArangoDB</b> - <i>open-source NoSQL database that supports key-value, document, and graph data models, created 2011</i>
+					<ul className="list-disc ml-6">
+						<li>currently have it installed on Windows, next step use with Go: <a href="https://tanguay-eu.vercel.app/forays/319" target="_blank" className="underline">https://tanguay-eu.vercel.app/forays/319</a></li>
+					</ul>
+				</li>
+			</ul>
+
+			<ul className="list-disc ml-6 mt-3">
+				<li><b>GraphQL</b> - <i>query language for APIs that allows clients to request exactly the data they need, released by Facebook 2015</i>
+					<ul className="list-disc ml-6">
+						<li>demonstration of GraphQL vs. REST : <a href="https://tanguay-eu.vercel.app/howtos/602" target="_blank" className="underline">https://tanguay-eu.vercel.app/howtos/602</a></li>
+					</ul>
+				</li>
+			</ul>
+
+			<ul className="list-disc ml-6 mt-3">
+				<li><b>Go</b> - <i>statically typed, compiled programming language and designed for simplicity, efficiency, and scalability, released by Google in 2009</i>
+					<ul className="list-disc ml-6">
+						<li>Go API frontend/backend with MongoDB database running on a Hetzner cloud machine: <a href="https://showcase-go-use-api.vercel.app" target="_blank" className="underline">https://showcase-go-use-api.vercel.app</a></li>
+					</ul>
+				</li>
+			</ul>
+
+			<p className="mt-4 text-orange-900">More notes, links and technologies which will eventually be merged into this project: <a href="https://tanguay-eu.vercel.app" target="_blank" className="underline">https://tanguay-eu.vercel.app</a> </p>
 
 		</section>
 	)
