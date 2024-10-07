62464d99ded4a5f34a7c1115ca0a41467a28647a
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-07 15:19:23 +0200

    toggles

--- src/components/PropTestBox.tsx
+++ src/components/PropTestBox.tsx
@@ -1,12 +1,15 @@
 interface IProps {
 	title: string;
 	visible: boolean;
+	toggleVisible: () => void;
 }
-export const PropTestBox = ({title, visible}: IProps) => {
+
+export const PropTestBox = ({title, visible, toggleVisible}: IProps) => {
 	return (
-		<div className="bg-gray-600 text-gray-200 mt-3 p-3 w-[12rem]">
+		<div className="bg-gray-800 text-gray-200 mt-3 p-3 w-[12rem]">
 			<h3 className="text-xl font-bold">{title}</h3>
 			<p>Value of visible is: <span className="text-yellow-300">{visible ? 'TRUE' : 'false'}</span></p>
+			<button onClick={toggleVisible}>toggle visible</button>
 		</div>
 	)
 }
--- src/pages/PagePropTest.tsx
+++ src/pages/PagePropTest.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import { PropTestBox } from "../components/PropTestBox"
 
 export const PagePropTest = () => {
+	const [showTheBox, setShowTheBox] = useState(true);
+
+	const toggleShowTheBox = () => {
+		setShowTheBox(!showTheBox);
+	}
+
 	return (
 		<>
 			<h2>Below is a component which changes a variable in its parent.</h2>
-			<PropTestBox title="This is the title" visible={false} />
+			<PropTestBox title="This is the title" visible={showTheBox} toggleVisible={toggleShowTheBox} />
 		</>
 	)
 }
