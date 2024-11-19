50d287d2da116d425d3f05b6358635b38018f82c
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-19 17:28:12 +0100

    string works

--- src/pages/PageChangeState.tsx
+++ src/pages/PageChangeState.tsx
@@ -1,5 +1,17 @@
+import { useTypedStoreActions, useTypedStoreState } from "../store/hooks";
+
 export const PageChangeState = () => {
+	const { testMessage } = useTypedStoreState((state) => state.settingsModel);
+	const { setTestMessage } = useTypedStoreActions((actions) => actions.settingsModel);
+
+	const handleChangeMessage = () => {
+		setTestMessage('CHANGED MESSAGE')
+	}
+
 	return (
-		<p>testing</p>
+		<>
+			<p>test message: {testMessage}</p>
+			<div className="mt-3"><button onClick={handleChangeMessage}>change message</button></div>
+		</>
 	)
 }
--- src/store/models/settingsModel.ts
+++ src/store/models/settingsModel.ts
@@ -2,13 +2,23 @@ import { Action, action } from "easy-peasy";
 
 export interface SettingsModel {
 	showInfoPage: boolean;
+	testMessage: string;
 
 	toggleIsOnline: Action<this>;
+	setTestMessage: Action<this, string>;
 }
 
 export const settingsModel: SettingsModel = {
+	// state
 	showInfoPage: false,
+	testMessage: 'ORIGINAL MESSAGE',
+	
+
+	// actions
 	toggleIsOnline: action((state) => {
 		state.showInfoPage = !state.showInfoPage;
+	}),
+	setTestMessage: action((state, message) => {
+		state.testMessage = message;
 	}),
 };
