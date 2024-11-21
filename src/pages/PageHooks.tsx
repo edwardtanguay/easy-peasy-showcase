b7a8958eb55f9d8a0195092fac728f08b390a4f1
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-21 02:18:05 +0100

    no color

--- src/pages/PageHooks.tsx
+++ src/pages/PageHooks.tsx
@@ -19,9 +19,12 @@ export const PageHooks = () => {
 						<span>{receiveNewsletter ? "send" : "no"}</span>
 					</div>
 					<div className="flex gap-2 w-[7rem]">
-						<input type="checkbox" onClick={toggleLocked}/>
-						<span>{isLocked ? "locked" : "not locked"}</span>
+						<label className="flex gap-2 cursor-pointer">
+							<input type="checkbox" checked={isLocked} onChange={toggleLocked} />
+							<span>{isLocked ? "locked" : "not locked"}</span>
+						</label>
 					</div>
+
 				</div>
 			</Example>
 		</>
