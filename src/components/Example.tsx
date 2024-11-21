ef4e56e445b87a7aa89f62a95aac8f5c2d99e0dc
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-21 02:06:16 +0100

    fieldset showing

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -3,8 +3,8 @@ interface IProps {
 }
 export const Example = ({ children }: IProps) => {
 	return (
-		<fieldset>
-			<legend>TEST</legend>
+		<fieldset className="border border-slate-500 px-4 pt-2 pb-3 w-fit rounded">
+			<legend className=" text-xs opacity-60">TEST</legend>
 			{children}
 		</fieldset>
 	)
