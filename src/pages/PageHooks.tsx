79070a45e00540354725ca9c4657b2e107d0b320
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-21 02:15:59 +0100

    thre ok

--- src/pages/PageHooks.tsx
+++ src/pages/PageHooks.tsx
@@ -10,16 +10,16 @@ export const PageHooks = () => {
 		<>
 			<Example title="useToggle">
 				<div className="flex gap-6">
-					<div className="flex gap-2">
+					<div className="flex gap-2 w-[6rem]">
 						<button onClick={toggleIsOnline}>Online</button>
 						<span>{isOnline ? "yes" : "no"}</span>
 					</div>
-					<div className="flex gap-2">
+					<div className="flex gap-2 w-[8rem]">
 						<button onClick={toggleNewsletter}>Newsletter</button>
-						<span>{receiveNewsletter ? "send" : "do not send"}</span>
+						<span>{receiveNewsletter ? "send" : "no"}</span>
 					</div>
-					<div className="flex gap-2">
-						<button onClick={toggleLocked}>Lock Status</button>
+					<div className="flex gap-2 w-[7rem]">
+						<input type="checkbox" onClick={toggleLocked}/>
 						<span>{isLocked ? "locked" : "not locked"}</span>
 					</div>
 				</div>
