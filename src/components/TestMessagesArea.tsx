f67c26eee23222b241673915b3fb1eea5aaa78ac
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-22 15:53:43 +0100

    easypeasy page all components

---
+++ src/components/TestMessagesArea.tsx
@@ -0,0 +1,30 @@
+import { useTypedStoreActions, useTypedStoreState } from "../store/hooks";
+import { Example } from "./Example";
+
+export const TestMessagesArea = () => {
+	const { testMessagesSearchText, filteredTestMessages } = useTypedStoreState((state) => state.testMessageModel);
+	const { addTestMessage, deleteTestMessage, resetTestMessages, handleChangeTestMessageSearchText } = useTypedStoreActions((actions) => actions.testMessageModel);
+
+	return (
+			<Example title="array of strings">
+				<form>
+					<div className="flex gap-3">
+						<button type="button" className="mb-3 normalButton" onClick={() => addTestMessage()}>add</button>
+						<button type="button" className="mb-3 normalButton" onClick={() => deleteTestMessage()}>delete</button>
+						<button type="button" className="mb-3 normalButton" onClick={() => resetTestMessages()}>reset</button>
+					</div>
+					<div className="mb-3">
+						<input placeholder="search" onChange={(e) => handleChangeTestMessageSearchText(e.target.value)} value={testMessagesSearchText} />
+					</div>
+				</form>
+				<p className="mb-2">There are {filteredTestMessages.length} test messages.</p>
+				<ul className="list-disc ml-6">
+					{filteredTestMessages.map((filteredTestMessage, index) => {
+						return (
+							<li key={index}>{filteredTestMessage}</li>
+						)
+					})}
+				</ul>
+			</Example>
+	)
+}
--- src/pages/PageEasyPeasy.tsx
+++ src/pages/PageEasyPeasy.tsx
@@ -1,38 +1,13 @@
-import { Example } from "../components/Example"
-import { useTypedStoreActions, useTypedStoreState } from "../store/hooks";
+import { EmployeesArea } from "../components/EmployeesArea";
+import { TestMessagesArea } from "../components/TestMessagesArea";
 
 export const PageEasyPeasy = () => {
-	const { testMessagesSearchText, filteredTestMessages } = useTypedStoreState((state) => state.testMessageModel);
-	const { addTestMessage, deleteTestMessage, resetTestMessages, handleChangeTestMessageSearchText } = useTypedStoreActions((actions) => actions.testMessageModel);
-	const {employees} = useTypedStoreState((state) => state.employeeModel);
-
 	return (
 		<>
 			<h2 className="text-2xl">Easy Peasy Showcase</h2>
 			<p className="mt-2 mb-3">This page shows examples of using Easy-Peasy Redux in various ways.</p>
-			<Example title="employee objects loaded via API with full CRUD functionality">
-				<p>{employees.length} employees</p>
-			</Example>
-			<Example title="array of strings">
-				<form>
-					<div className="flex gap-3">
-						<button type="button" className="mb-3 normalButton" onClick={() => addTestMessage()}>add</button>
-						<button type="button" className="mb-3 normalButton" onClick={() => deleteTestMessage()}>delete</button>
-						<button type="button" className="mb-3 normalButton" onClick={() => resetTestMessages()}>reset</button>
-					</div>
-					<div className="mb-3">
-						<input placeholder="search" onChange={(e) => handleChangeTestMessageSearchText(e.target.value)} value={testMessagesSearchText} />
-					</div>
-				</form>
-				<p className="mb-2">There are {filteredTestMessages.length} test messages.</p>
-				<ul className="list-disc ml-6">
-					{filteredTestMessages.map((filteredTestMessage, index) => {
-						return (
-							<li key={index}>{filteredTestMessage}</li>
-						)
-					})}
-				</ul>
-			</Example>
+			<EmployeesArea />
+			<TestMessagesArea />
 		</>
 	)
 }
