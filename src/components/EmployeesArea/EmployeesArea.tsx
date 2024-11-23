fbfa81ed7baab2725024512770a6189fb14d10ce
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-23 13:21:53 +0100

    no records message

--- src/components/EmployeesArea/EmployeesArea.tsx
+++ src/components/EmployeesArea/EmployeesArea.tsx
@@ -10,63 +10,70 @@ export const EmployeesArea = () => {
 
 	return (
 		<section className="pageEasyPeasy">
-			{loadingStatus === "finished" && (
-				<form className="my-2 flex gap-1">
-					<input placeholder="search" autoFocus={true} value={searchText} onChange={(e) => handleSearchTextChange(e.target.value)} className="bg-gray-300 rounded p-1 text-lg" />
-					{searchText !== "" && (
-						<div className="p-[.1rem]">
-							<MdOutlineCancel className="text-slate-500 text-[2rem] hover:text-slate-600 cursor-pointer" onClick={() => handleCancelSearch()} />
-						</div>
-					)}
-				</form>
-			)}
 			{(loadingStatus === "readyToLoad" || loadingStatus === "loading") && (
 				<WaitSpinner />
 			)}
 			{loadingStatus === "error" && (
 				<p className="text-red-800 italic">Sorry, the data couldn't be loaded, please contact the website administrator.</p>
 			)}
-			<table className={`employees ${loadingStatus === 'finished' ? 'fadeIn' : 'fadeOut'}`}>
-				{loadingStatus === "finished" && (
-					<>
-						<thead>
-							<tr>
-								{showIds && (
-									<th className="dpId">dpodid</th>
-								)}
-								<th onClick={() => handleChangeSort("firstName")}><span>First Name</span></th>
-								<th onClick={() => handleChangeSort("lastName")}><span>Last Name</span></th>
-								<th onClick={() => handleChangeSort("dateOfBirth")}><span>Date of Birth</span></th>
-								<th onClick={() => handleChangeSort("department")}><span>Department</span></th>
-								<th onClick={() => handleChangeSort("isActive")}><span>Is Active</span></th>
-								<th onClick={() => handleChangeSort("yearsOfExperience")}><span>Years Experience</span></th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							{filteredEmployees.map(employee => {
-								return (
-									<tr key={employee.id}>
-										{showIds && (
-											<td className="dpId">{employee.dpodId}</td>
-										)}
-										<td className="dpLine">{employee.firstName}</td>
-										<td className="dpLine">{employee.lastName}</td>
-										<td className="dpDate">{employee.dateOfBirth}</td>
-										<td className="dpLine">{employee.department}</td>
-										<td className="dpYesNo">{employee.isActive ? 'yes' : 'no'}</td>
-										<td className="dpWholeNumber">{employee.yearsOfExperience}</td>
-										<td><RiDeleteBin6Line className="cursor-pointer text-red-900 hover:text-red-700" /></td>
-									</tr>
-								)
-							})}
-						</tbody>
-						<tfoot>
-							<span className="text-xs ml-1 text-gray-700 hover:underline cursor-pointer" onClick={() => handleToggleShowIds()}>show ids</span>
-						</tfoot>
-					</>
-				)}
-			</table>
+			{loadingStatus === "finished" && (
+				<>
+					<form className="my-2 flex gap-1">
+						<input placeholder="search" autoFocus={true} value={searchText} onChange={(e) => handleSearchTextChange(e.target.value)} className="bg-gray-300 rounded p-1 text-lg" />
+						{searchText !== "" && (
+							<div className="p-[.1rem]">
+								<MdOutlineCancel className="text-slate-500 text-[2rem] hover:text-slate-600 cursor-pointer" onClick={() => handleCancelSearch()} />
+							</div>
+						)}
+					</form>
+					{filteredEmployees.length === 0 && (
+						<p className="mt-5 text-2xl text-gray-600 italic">No records match your search.</p>
+					)}
+					{filteredEmployees.length > 0 && (
+						<table className={`employees ${loadingStatus === 'finished' ? 'fadeIn' : 'fadeOut'}`}>
+							{loadingStatus === "finished" && (
+								<>
+									<thead>
+										<tr>
+											{showIds && (
+												<th className="dpId">dpodid</th>
+											)}
+											<th onClick={() => handleChangeSort("firstName")}><span>First Name</span></th>
+											<th onClick={() => handleChangeSort("lastName")}><span>Last Name</span></th>
+											<th onClick={() => handleChangeSort("dateOfBirth")}><span>Date of Birth</span></th>
+											<th onClick={() => handleChangeSort("department")}><span>Department</span></th>
+											<th onClick={() => handleChangeSort("isActive")}><span>Is Active</span></th>
+											<th onClick={() => handleChangeSort("yearsOfExperience")}><span>Years Experience</span></th>
+											<th></th>
+										</tr>
+									</thead>
+									<tbody>
+										{filteredEmployees.map(employee => {
+											return (
+												<tr key={employee.id}>
+													{showIds && (
+														<td className="dpId">{employee.dpodId}</td>
+													)}
+													<td className="dpLine">{employee.firstName}</td>
+													<td className="dpLine">{employee.lastName}</td>
+													<td className="dpDate">{employee.dateOfBirth}</td>
+													<td className="dpLine">{employee.department}</td>
+													<td className="dpYesNo">{employee.isActive ? 'yes' : 'no'}</td>
+													<td className="dpWholeNumber">{employee.yearsOfExperience}</td>
+													<td><RiDeleteBin6Line className="cursor-pointer text-red-900 hover:text-red-700" /></td>
+												</tr>
+											)
+										})}
+									</tbody>
+									<tfoot>
+										<span className="text-xs ml-1 text-gray-700 hover:underline cursor-pointer" onClick={() => handleToggleShowIds()}>show ids</span>
+									</tfoot>
+								</>
+							)}
+						</table>
+					)}
+				</>
+			)}
 		</section>
 	)
 }
